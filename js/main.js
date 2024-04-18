26ca32a87265b4756da5d2456acf1b6e0379c7da
alexandreValenzuela <richard.valenzuela.et7@gmail.com>
2024-04-18 19:37:58 -0300

    trabjae con java scrip

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let nombreProducto = "mapa"
+let precioUnitario = 120
+let cantidadDeseada = prompt("ingrese la cantidad de mapas deseada")
+let costoTotal = precioUnitario * cantidadDeseada
+
+alert(costoTotal)
